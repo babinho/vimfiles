4c454fa87a436bb4478f9b2b9e6a5bc4bffdd7dd
babinho <babinho@gmail.com>
2012-11-06 18:32:22 +0100

    added font install script to install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 git submodule update --init
 ln -s ~/.vim/vimrc ~/.vimrc
+
+# this installs fonts
+sudo cp -R ./fonts/source-code-pro-powerline /usr/share/fonts/truetype
+sudo fc-cache -fv
